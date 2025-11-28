90e3c7f9e3df5e6bb57a514cdd294b782d51ef62
Sridhar Ratnakumar <srid@srid.ca>
2025-11-27 19:23:58 -0500

    use it

--- packages/ob/src/Ob/Vault.hs
+++ packages/ob/src/Ob/Vault.hs
@@ -7,10 +7,15 @@ module Ob.Vault (
 )
 where
 
-import Effectful
-import Effectful.Colog.Simple (Severity (..), log, runLogActionStdout)
+import Effectful (Eff, IOE, withEffToIO, (:>))
+import Effectful qualified
+import Effectful.Colog (Log)
+import Effectful.Colog.Simple (LogContext, Severity (..), log, runLogActionStdout, withLogContext)
+import Effectful.Internal.Monad (Limit (Unlimited), Persistence (Ephemeral), UnliftStrategy (ConcUnlift))
+import Effectful.Reader.Static qualified as ER
 
-import Colog (LogAction (..))
+import Colog (LogAction (..), WithSeverity (..))
+import Colog.Message (RichMessage)
 import Data.LVar (LVar)
 import Data.LVar qualified as LVar
 import Data.Map.Strict qualified as Map
@@ -31,10 +36,11 @@ getTasks vault = concatMap tasks (Map.elems vault.notes)
 -- | Like `withVault` but returns the current snapshot, without monitoring it.
 getVault :: FilePath -> IO Vault
 getVault path = do
-  runEff . runLogActionStdout Info $ do
-    (notesMap, _) <- mountVault path
-    log Info $ "Model ready; initial docs = " <> show (Map.size notesMap) <> "; sample = " <> show (take 4 $ Map.keys notesMap)
-    pure $ Vault notesMap
+  Effectful.runEff . runLogActionStdout Info $ do
+    Effectful.Colog.Simple.withLogContext [("context", "Vault")] $ do
+      (notesMap, _) <- mountVault path
+      log Info $ "Model ready; initial docs = " <> show (Map.size notesMap) <> "; sample = " <> show (take 4 $ Map.keys notesMap)
+      pure $ Vault notesMap
 
 {- | Calls `f` with a `LVar` of `Vault` reflecting its current state in real-time.
 
@@ -42,29 +48,31 @@ Uses `System.UnionMount` to monitor the filesystem for changes.
 -}
 withLiveVault :: FilePath -> (LVar Vault -> IO ()) -> IO ()
 withLiveVault path f = do
-  runEff . runLogActionStdout Info $ do
-    (notesMap0, modelF) <- mountVault path
-    let initialVault = Vault notesMap0
-    log Info $ "Model ready; total docs = " <> show (Map.size notesMap0)
-    modelVar <- LVar.new initialVault
-    liftIO $ concurrently_ (f modelVar) $ do
-      modelF $ \newNotesMap -> do
-        runEff . runLogActionStdout Info $ do
-          let newVault = Vault newNotesMap
-          let newTasks = getTasks newVault
-          log Info $ "Model updated; total docs = " <> show (Map.size newNotesMap) <> "; total tasks = " <> show (length newTasks)
-          liftIO $ LVar.set modelVar newVault
+  Effectful.runEff . runLogActionStdout Info $ do
+    Effectful.Colog.Simple.withLogContext [("context", "Vault")] $ do
+      (notesMap0, modelF) <- mountVault path
+      let initialVault = Vault notesMap0
+      log Info $ "Model ready; total docs = " <> show (Map.size notesMap0)
+      modelVar <- LVar.new initialVault
+      liftIO $ concurrently_ (f modelVar) $ do
+        modelF $ \newNotesMap -> do
+          Effectful.runEff . runLogActionStdout Info $ do
+            Effectful.Colog.Simple.withLogContext [("context", "Vault")] $ do
+              let newVault = Vault newNotesMap
+              let newTasks = getTasks newVault
+              log Info $ "Model updated; total docs = " <> show (Map.size newNotesMap) <> "; total tasks = " <> show (length newTasks)
+              liftIO $ LVar.set modelVar newVault
 
 mountVault ::
-  (IOE :> es) =>
+  (ER.Reader LogContext :> es, Log (RichMessage IO) :> es, IOE :> es) =>
   FilePath ->
   Eff
     es
     ( Map FilePath Note
     , (Map FilePath Note -> IO ()) -> IO ()
     )
-mountVault path = liftIO $ do
-  let logger = LogAction $ \msg -> putTextLn $ "ℹ️  INFO [UnionMount] " <> msg
+mountVault path = withEffToIO (ConcUnlift Ephemeral Unlimited) $ \runInIO -> do
+  let logger = LogAction $ \(WithSeverity msg sev) -> runInIO $ log sev msg
   (initial, umCallback) <- UM.mount logger path (one ((), "**/*.md")) ["**/.*/**"] mempty (const $ handleMarkdownFile path)
   let finalCallback userAction = do
         umCallback $ \notes -> liftIO $ userAction notes
