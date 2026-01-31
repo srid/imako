868287d82c39bbdccea88febd86d57ae6f6ace3e
Sridhar Ratnakumar <srid@srid.ca>
2026-01-31 17:30:37 -0500

    rm

--- packages/imako/src/Imako/Core/Filter.hs
+++ packages/imako/src/Imako/Core/Filter.hs
@@ -1,33 +1,18 @@
-{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE DeriveAnyClass #-}
 
 module Imako.Core.Filter where
 
-import Data.Aeson (ToJSON (..), object, (.=))
-import Data.Time (Day, addDays)
-import Ob (Task (..))
-import Ob.Task (TaskStatus (..))
-import Ob.Task.Properties (TaskProperties (..))
-import Text.Show (Show (show))
+import Data.Aeson (ToJSON)
 
--- | A filter for task visibility
+-- | A filter for task visibility (filtering happens client-side)
 data Filter = Filter
   { filterId :: Text
   -- ^ Unique identifier for the filter (camelCase, used as storage key)
   , filterLabel :: Text
   -- ^ Human-readable label for the filter button
-  , filterPredicate :: Day -> Task -> Bool
-  -- ^ Predicate function to determine if a task matches this filter
   }
-
--- Manual instances since we can't derive for functions
-instance Show Filter where
-  show f = "Filter {filterId = \"" <> toString f.filterId <> "\", filterLabel = \"" <> toString f.filterLabel <> "\"}"
-
-instance Eq Filter where
-  (==) f1 f2 = f1.filterId == f2.filterId
-
-instance ToJSON Filter where
-  toJSON f = object ["filterId" .= f.filterId, "filterLabel" .= f.filterLabel]
+  deriving stock (Show, Eq, Generic)
+  deriving anyclass (ToJSON)
 
 -- | All available filters
 filters :: [Filter]
@@ -35,17 +20,9 @@ filters =
   [ Filter
       { filterId = "showFuture"
       , filterLabel = "Future tasks"
-      , filterPredicate = \today task ->
-          let twoDaysFromNow = addDays 2 today
-              isFuture = case task.properties.startDate of
-                Just d -> d >= twoDaysFromNow
-                Nothing -> False
-              isParentFuture = any (\(_, start) -> maybe False (>= twoDaysFromNow) start) task.parentContext
-           in isFuture || isParentFuture
       }
   , Filter
       { filterId = "showPast"
       , filterLabel = "Past tasks"
-      , filterPredicate = \_ task -> task.status == Completed || task.status == Cancelled
       }
   ]
