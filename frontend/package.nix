a25817d398d84941b34fb5466d28c281b2cf11a5
Sridhar Ratnakumar <srid@srid.ca>
2026-02-01 11:49:08 -0500

    fix hash

--- frontend/package.nix
+++ frontend/package.nix
@@ -3,7 +3,7 @@ pkgs.buildNpmPackage {
   pname = "imako-frontend";
   version = "0.0.1";
   src = ./.;
-  npmDepsHash = "sha256-B76SQh8ywqYFERPXIIbeLMgaPau64+7RqY16E8O/OeY=";
+  npmDepsHash = "sha256-HqE3Bvw3bggDpOPn4kimlQ0wZ0Ze+gHGS3DWnwzYTKM=";
   buildPhase = ''
     npm run build
   '';
